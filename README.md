5e0da2a29157b0ca417f24a282eba1f80c69fbc4
llDenll <Dens.030206@gmail.com>
2023-03-03 22:01:11 +0200

    seccessful

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # telegram-bot
 https://github.com/llDenll/telegram-bot
+
+Eeee у мене вийшло
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print(".")
+
+
+def string():
+    print('Якщо ти не носиш string, то це ганьба')
+
+
+string()
